0d9d7101756f2252511bae8b20154fe6cdce74a3
samandarsxdev <93664571+samandarsxdev@users.noreply.github.com>
2021-12-17 11:04:19 -0500

    add

--- main.js
+++
@@ -1,9 +0,0 @@
-let money  = prompt("Alisherda necha som bor")-0;
-let sum = 9433.34*750;
-let sum1 = 10354.03*120;
-let number = sum + sum1;
- if(money == number){
-     console.log( "Oq yo’l, Alisher!" );
- }else{
-     console.log( "Alisher, ozgina sabr qilish kerak bo’lar ekan." );
- }
